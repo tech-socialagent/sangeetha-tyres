9e9d0161f40fa0b8f9f69e4498266fbd7ffc8303
Pavan Kumar <pavankumarts2001@gmail.com>
2023-10-05 10:32:45 +0530

    next config file update

--- next.config.js
+++ next.config.js
@@ -3,16 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
-
 const withVideos = require('next-videos')
 
-
-
-module.exports = withVideos()
-
 module.exports = {
+  ...nextConfig,
+  ...withVideos(),
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+      domains: ['firebasestorage.googleapis.com'],
   },
 };
