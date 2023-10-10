b62bd7661343312e2ad775121e2f0f6e79a84460
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-10-10 19:00:42 +0530

    single product page

--- src/pages/products/[product].js
+++ src/pages/products/[product].js
@@ -2,14 +2,39 @@ import React from 'react'
 import styles from '@/styles/Products/IndividualProduct.module.css'
 import Navbar from '@/Components/Common/Navbar'
 import Footer from '@/Components/Common/Footer'
+import SingleProduct from '@/Components/Products/SingleProduct'
 
 const IndividualProduct = () => {
+
+    const productData = {
+        id:'135/70R12',
+        name:'Sturdo passenger',
+        brandImage:'/assests/home/logos/image1.png',
+        mrp:'4,999.00',
+        images:[
+            "/assests/Products/demo/demo1.png",
+            "/assests/Products/demo/demo2.png",
+            "/assests/Products/demo/demo3.png",
+            "/assests/Products/demo/demo4.png"
+        ],
+        width:'165',
+        rim:'14',
+        speed:'T',
+        ratio:'70',
+        loadIndex:'81',
+        tubeless: true,
+
+        //Section Header
+        title:'BRIDGESTONE - STRUDO PASSENGER'
+    };
+
     return (
         <div>
             <Navbar />
             <div className={styles.banner}>
                 <h3>BRIDGESTONE TYRE CATEGORIES</h3>
             </div>
+            <SingleProduct data={productData}/>
             <Footer />
         </div>
     )
