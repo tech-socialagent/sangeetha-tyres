4927b8c235d71151fa8c0a987c55165db3a7e63a
Pavan Kumar <pavankumarts2001@gmail.com>
2023-11-15 15:19:03 +0530

    loding skeleton

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,33 @@
 import Footer from '@/Components/Common/Footer'
 import Navbar from '@/Components/Common/Navbar'
+import Loading from '@/Components/loading'
 import '@/styles/globals.css'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  // const [route, setRoute] = useState(router.pathname)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    console.log("router.pathname", router);
+    if (router.asPath != '/#contactUs') {
+      setLoading(true)
+      setTimeout(async () => {
+        setLoading(false);
+      }, 1000);
+    }
+    else {
+      setLoading(false);
+    }
+  }, [router.pathname])
+
   return (
     <>
-      {/* <Navbar /> */}
-      <Component {...pageProps} />
-      {/* <Footer /> */}
+      {
+        loading ? <Loading /> : <Component {...pageProps} />
+      }
     </>
   )
 }
